eda1e7286ede41c72b9531ddd5e4c887c89d2816
Esoteric918 <85411745+Esoteric918@users.noreply.github.com>
2021-09-16 21:37:00 -0500

    bullble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting algorithm
+ * @array: array of data to be sorted
+ * @size: size of the array
+ * Return: Nothing
+ * 
+ */
+void bubble_sort (int *array, size_t size)
+{
+    size_t i, j;
+    int swap;
+    if (array > 2)
+        return;
+    for (i = 0; c < size - 1; ++i)
+    {
+        for (j = 0; j < size - i - 1; ++j)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = swap;
+                print_array(array, size);   
+            }
+        }
+    }
+}
