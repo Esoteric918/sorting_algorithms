b3c7415c8db87fae342253788c3c0731ae8ae32c
Esoteric918 <3411@holbertonschool.com>
2021-09-20 02:53:49 -0700

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,25 +5,25 @@
  * @array: array of data to be sorted
  * @size: size of the array
  * Return: Nothing
- * 
  */
-void bubble_sort (int *array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int swap;
-    if (size < 2)
-        return;
-    for (i = 0; i < size - 1; ++i)
-    {
-        for (j = 0; j < size - i - 1; ++j)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = swap;
-                print_array(array, size);   
-            }
-        }
-    }
+size_t i, j;
+int swap;
+
+if (size < 2)
+return;
+for (i = 0; i < size - 1; ++i)
+{
+for (j = 0; j < size - i - 1; ++j)
+{
+if (array[j] > array[j + 1]
+{
+swap = array[j];
+array[j] = array[j + 1];
+array[j + 1] = swap;
+print_array(array, size)
+}
+}
+}
 }
