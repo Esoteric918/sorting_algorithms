28c2052191bfac3930032bbc5062875db6cca4a4
Esoteric918 <3411@holbertonschool.com>
2021-09-20 04:43:09 -0700

    seletion sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort algo
+ * @array: integer array
+ * @size: size_t
+ * Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, mi;
+	int min;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		mi = i;
+		for (j = i; j < size; j++)
+		{	if (array[j] < min)
+			{
+				min = array[j];
+				mi = j;
+			}
+		if (i != mi)
+		{
+			array[i] += array[mi];
+			array[mi] = array[i] - array[mi];
+			array[i] -= array[mi];
+			print_array(array, size);
+		}
+		}
+	}
+}
