8c6fc8a2391f7242b885d2b0ff92bcd174a20c65
Esoteric918 <3411@holbertonschool.com>
2022-02-28 13:11:36 -0600

    betty fix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -5,21 +5,22 @@
  * @arr: array
  * @n: size of array
  *
- * Returns: int
+ * Return: int
  */
 
 int getMax(int arr[], int n)
 {
 	int i;
 	int max = arr[0];
-	for ( i = 1; i < n ; ++i)
+
+	for (i = 1; i < n ; ++i)
 		if (arr[i] > max)
 			max = arr[i];
 	return (max);
 }
 
 /**
- * countSort sorts the arr in LSD
+ * countingSort - sorts the arr in LSD
  *@array: array to be sorted
  *@size: size of the array
  *@lsd: least signifcant digit
@@ -32,24 +33,26 @@ void countingSort(int *array, size_t size, int *buff, int lsd)
 	size_t idx;
 
 	for (idx = 0; idx < size; idx++)
+	{
 		arrCount[(array[idx] / lsd) % 10]++;
-
+	}
 	for (idx = 1; idx < 10; idx++)
-		 arrCount[idx] += arrCount[idx - 1];
-
+	{
+		arrCount[idx] += arrCount[idx - 1];
+	}
 	for (idx = size - 1; (int)idx >= 0; idx--)
 	{
 		buff[arrCount[(array[idx] / lsd) % 10] - 1] = array[idx];
 		arrCount[(array[idx] / lsd) % 10]--;
-  	}
+	}
 
-  	for (idx = 0; idx < size; ++idx)
-    	array[idx] = buff[idx];
+	for (idx = 0; idx < size; ++idx)
+		array[idx] = buff[idx];
 }
 
 
 /**
- * radix_sort
+ * radix_sort - sorts an array
  *
  * @array: the arr to be sorted
  * @size: the size of the arr
@@ -63,13 +66,13 @@ void radix_sort(int *array, size_t size)
 	int max = getMax(array, size);
 	int *temp = malloc(sizeof(int) * size);
 
-	if (array == NULL || size < 2 || temp ==NULL)
+	if (array == NULL || size < 2 || temp == NULL)
 	{
 		free(temp);
 		return;
 	}
 	for (lsd = 1; max / lsd > 0; lsd *= 10)
-    {
+	{
 		countingSort(array, size, temp, lsd);
 		print_array(array, size);
 	}
